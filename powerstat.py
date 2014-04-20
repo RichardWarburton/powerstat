d5abaa950e5360a655b5bac528e6b5c18f4a8a92
Richard Warburton <richard.warburton@gmail.com>
2014-04-20 22:39:47 +0100

    adds auto-detection of batteries and better formatting

--- powerstat.py
+++ powerstat.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 
-# TODO: detect batteries
 
 from time import sleep
 from datetime import timedelta
+from os import listdir
+from os.path import join
+import sys
 
-POLL_PERIOD = 10.0
+POWER_SUPPLIES = "/sys/class/power_supply/"
+DEFAULT_POLL_PERIOD = 1.0
+SAMPLES = 10
 
-prefix = '/sys/devices/LNXSYSTM:00/device:00/PNP0A08:00/device:09/PNP0C09:00/'
-batteries = [prefix + 'PNP0C0A:00/power_supply/BAT0', prefix + 'PNP0C0A:01/power_supply/BAT1']
+def detect_batteries():
+    batteries = [join(POWER_SUPPLIES, bat) for bat in listdir(POWER_SUPPLIES) if bat.startswith('BAT')]
+    if not batteries:
+        sys.exit(1)
+    
+    print "Detected {0} batteries".format(len(batteries))
+    return batteries
 
 def read_now(directory):
     return read_file(directory + '/energy_now')
@@ -24,18 +33,49 @@ def delta(value):
     (current, last) = value
     return last - current
 
-fulls = map(read_full, batteries)
-last = None
+def print_delta(seconds):
+    if seconds == 0:
+        print "No change"
+        return
 
-while True:
-    current = map(read_now, batteries)
-    if last:
-        deltas = map(delta, zip(current, last))
-        values = filter(lambda x: x > 0, deltas)
-        if values:
-            total = float(sum(current))
-            rate = float(values[0]) / POLL_PERIOD
-            print str(timedelta(seconds = total / rate))
-    last = current
-    sleep(POLL_PERIOD)
+    if seconds < 0:
+        prefix = "Charged in"
+    else:
+        prefix = "Drained in"
+    
+    print prefix, str(timedelta(seconds=abs(seconds)))
+
+def update_history(history, new_value):
+    history.append(new_value)
+    if len(history) > SAMPLES:
+        history = history[1:]
+
+def output(history):
+    if history:
+        seconds = sum(history) / len(history)
+        print_delta(seconds)
+
+def main():
+    batteries = detect_batteries()
+    POLL_PERIOD = float(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_POLL_PERIOD
+    history = []
+    fulls = map(read_full, batteries)
+    last = None
+    
+    while True:
+        current = map(read_now, batteries)
+        if last:
+            deltas = map(delta, zip(current, last))
+            values = filter(lambda x: x != 0, deltas)
+            if values:
+                total = float(sum(current))
+                rate = float(values[0]) / POLL_PERIOD
+                update_history(history, total / rate)
+
+        output(history)
+        last = current
+        sleep(POLL_PERIOD)
+
+if __name__ == "__main__":
+    main()
 
